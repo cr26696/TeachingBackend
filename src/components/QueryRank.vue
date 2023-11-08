e45375b599b6a159d6a3d76bec343e3846c54350
imbaXmz0121 <imbaXmz@gmail.com>
2023-11-08 20:55:07 +0800

    忘了一处

--- src/components/QueryRank.vue
+++ src/components/QueryRank.vue
@@ -2,10 +2,13 @@
 <div>
   <el-container>
         <el-aside id="achievement-side">
-            <el-menu>
-                <el-menu-item index="1">成果录入申请</el-menu-item>
-                <el-menu-item index="2">成果填写</el-menu-item>
-                <el-menu-item index="3">全部成果</el-menu-item>
+            <el-menu :default-openeds="['1']">
+                <el-submenu index="1">
+                        <template slot="title">学评教排名</template>
+                        <el-menu-item index="1-1">学校排名</el-menu-item>
+                        <el-menu-item index="1-2">学院排名</el-menu-item>
+                    </el-submenu>
+                <el-menu-item index="2">教学业绩考核</el-menu-item>
             </el-menu>
         </el-aside>
         <el-main></el-main>
